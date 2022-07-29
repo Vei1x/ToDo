2732f12929e79d3fc7a62017d089fb6242de7823
nikita.hvoina <nikita.hvoina@accenture.com>
2022-07-29 06:43:43 +0300

    /feature: add controller test

--- src/test/java/com/nikita/todo/business/service/impl/TodoTodoTaskServiceImplTest.java
+++ src/test/java/com/nikita/todo/business/service/impl/TodoTodoTaskServiceImplTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class TaskServiceImplTest {
+class TodoTodoTaskServiceImplTest {
 
     @Test
     void getAllTasks() {
